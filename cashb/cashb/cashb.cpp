23febc96711a9d66facc3e360263969f9b175fbd
Alexandr Moskvichev <118541543+wqrxysf@users.noreply.github.com>
2025-05-23 21:34:36 +0300

    add first part of mlata labs

---
+++ cashb/cashb/cashb.cpp
@@ -0,0 +1,63 @@
+﻿/*
+15.6. Касса (5)
+Написать программу для игры касса, рассмотренной в настоящем разделе.
+Ввод из файла INPUT.TXT. В единственной строке находится количество монет N (N ≤ 200).
+Вывод в файл OUTPUT.TXT. Вывести 1, если при правильной игре выигрывает первый игрок, либо 2 – если выигрывает второй.
+
+Москвичев А.Е. ПС-22
+Visual Studio 2022
+*/
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+const int MAX_N = 200;
+
+int main() {
+    ifstream fin("INPUT.TXT");
+    ofstream fout("OUTPUT.TXT");
+
+    int N;
+    fin >> N;
+
+    // S[K][M]
+    int maxM = N + 1;
+    vector<vector<int>> S(N + 1, vector<int>(maxM + 1, 0));
+
+    // K <= 2M можно забрать всё
+    for (int K = 0; K <= N; ++K) {
+        for (int M = 1; M <= maxM; ++M) {
+            if (K <= 2 * M) {
+                S[K][M] = K;
+            }
+        }
+    }
+
+    for (int K = 3; K <= N; ++K) {
+        for (int M = 1; M <= maxM; ++M) {
+            if (K <= 2 * M) continue;
+            bool losingPosition = true;
+            for (int L = 1; L <= 2 * M; ++L) {
+                if (K - L >= 0 && S[K - L][L] == 0) {
+                    S[K][M] = L;
+                    losingPosition = false;
+                    break;
+                }
+            }
+            if (losingPosition) {
+                S[K][M] = 0;
+            }
+        }
+    }
+
+    if (S[N][1] > 0) {
+        fout << 1 << endl;
+    }
+    else {
+        fout << 2 << endl;
+    }
+
+    return 0;
+}
---
+++ changemoney/changemoney/changemoney.cpp
@@ -0,0 +1,74 @@
+﻿/*
+Требуется разменять заданную сумму денег минимальным количеством монет.
+Ввод. Первая строка файла INPUT.TXT содержит сумму денег L (1 ≤ L ≤ 20000) и количество типов монет N (1 ≤ N ≤ 30). 
+Вторая строка содержит N значений Ci через пробел: стоимости типов монет (1 ≤ Ci ≤ L).
+
+Вывод. В первой строке выводится количество типов монет K, необходимое для размена, и общее минимальное количество монет M. 
+В следующих K строках выводятся через пробел по 2 числа: стоимость монеты и количество экземпляров данной монеты. 
+Эти K строк должны выводится в порядке убывания стоимости монет. Если имеется несколько вариантов решения, достаточно вывести любой из них. 
+Если указанную сумму разменять невозможно, то выходной файл должен содержать No.
+
+Москвичев А.Е. ПС-22
+Visual Studio 2022
+*/
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+int main() {
+    ifstream in("INPUT.TXT");
+    ofstream out("OUTPUT.TXT");
+
+    int L, N;
+    in >> L >> N;
+    vector<int> C(N);
+    for (int i = 0; i < N; ++i) {
+        in >> C[i];
+    }
+
+    vector<int> dp(L + 1, INT_MAX);
+    vector<vector<int>> count(L + 1, vector<int>(N, 0));
+
+    dp[0] = 0;
+    for (int i = 1; i <= L; ++i) {
+        for (int j = 0; j < N; ++j) {
+            if (C[j] <= i && dp[i - C[j]] != INT_MAX) {
+                if (dp[i - C[j]] + 1 < dp[i]) {
+                    dp[i] = dp[i - C[j]] + 1;
+                    count[i] = count[i - C[j]];
+                    count[i][j]++;
+                }
+            }
+        }
+    }
+
+    if (dp[L] == INT_MAX) {
+        out << "No" << endl;
+    }
+    else {
+        int K = 0;
+        for (int j = 0; j < N; ++j) {
+            if (count[L][j] > 0) K++;
+        }
+
+        out << K << " " << dp[L] << endl;
+
+        vector<pair<int, int>> result;
+        for (int j = 0; j < N; ++j) {
+            if (count[L][j] > 0) {
+                result.emplace_back(C[j], count[L][j]);
+            }
+        }
+        sort(result.rbegin(), result.rend());
+        for (const auto& p : result) {
+            out << p.first << " " << p.second << endl;
+        }
+    }
+
+    return 0;
+}
